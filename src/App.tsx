84ab942a9519872c904a8833e4d8ae9ff74c1b4c
Araya Pudtal <natt.araya@gmail.com>
2025-05-23 10:28:59 +0700

    update title home page

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>E-Com System 2.0.1 fixed bug (update-label-cart-button) (add grid ui) </h1>
+      <h1>E-Com System 7.0.0 Test CI (Workflows) </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
